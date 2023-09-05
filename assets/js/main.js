0ff4ab3d45b5b3e603a325e542f16420bd962f75
luizfellipes <luiz.13.silva@gmail.com>
2023-09-05 10:37:14 -0400

    Foi convertido todo o for do JS para map, simplificando a função pokeApi.getPokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,5 @@
 
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?${limit}=10&offset=${offset}`;
-
-
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
@@ -24,15 +19,6 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList');
 
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon);
-        }
-
-    })
-    .catch((error) => console.error(error));
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+});
